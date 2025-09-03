6f9d1af86e3d15f66f3ff9d1f9c34680d014622a
Exalaxe <alexanderjacob1107aj@gmail.com>
2025-09-03 23:46:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 
 const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(cors());
 app.use(express.json());
 
@@ -44,5 +47,6 @@ app.get("/load/:playerId", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 

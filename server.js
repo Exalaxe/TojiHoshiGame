d39ba1a720f8f5f125640ae0c597bf643d6d88e1
Exalaxe <alexanderjacob1107aj@gmail.com>
2025-09-03 23:24:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.get("/load/:playerId", async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
